735d248f5ae808dc36a5735cf024b09c8e69c4f2
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-05 16:36:08 +0700

    User controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grubhub.Controllers
+{
+	public class UserController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
