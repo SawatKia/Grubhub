6dde731fa8949df007db756b8e2741408311ac30
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-07 21:00:43 +0700

    Users model

---
+++ Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.ComponentModel.DataAnnotations;
+
+namespace Grubhub.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Date Created")]
+        [DataType(DataType.DateTime)]
+        public DateTime Date_Created { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [AllowNull]
+        public string Roles { get; set; } //Admin, Grabber, Customer
+
+        
+    }
+}
