5ba79dd3053fa8a46042a18211f1574897e6bf3f
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-09 23:33:39 +0700

    database config

---
+++ Data/GrubhubDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Grubhub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Grubhub.Data
+{
+    public class GrubhubDBContext:DbContext
+    {
+        //constutor
+        public GrubhubDBContext(DbContextOptions<GrubhubDBContext> options) : base(options) { 
+
+        }
+        //create UsersData table in DB reference column from model User
+        public DbSet<User> UsersData { get; set; }
+    }
+}
