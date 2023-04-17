86dd6bbdfdc125506d1014931b9b49cbae727302
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-17 20:06:44 +0700

    add Grabber Model

---
+++ Models/GrabberPost.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Grubhub.Models
+{
+    public class GrabberPost
+    {
+        [Key]
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Date_Created { get; set; } = DateTime.Now;
+
+        [Required]
+        [StringLength(100)]
+        public string CanteenName { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int MaxQuantity { get; set; }
+
+        [Required]
+        [Range(1, double.MaxValue)]
+        public double MaxPrice { get; set; }
+
+        [Required]
+        [AllowNull]
+        public DateTime? CloseTime { get; set; }
+
+    }
+}
