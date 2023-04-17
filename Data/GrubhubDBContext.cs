af7ae02dc87d5b181cc013fcbdffa21dc025b3c2
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-17 20:06:17 +0700

    add GrabberPostingField table

--- Data/GrubhubDBContext.cs
+++ Data/GrubhubDBContext.cs
@@ -3,13 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Grubhub.Data
 {
-    public class GrubhubDBContext:DbContext
+    public class GrubhubDBContext : DbContext
     {
         //constutor
-        public GrubhubDBContext(DbContextOptions<GrubhubDBContext> options) : base(options) { 
+        public GrubhubDBContext(DbContextOptions<GrubhubDBContext> options) : base(options)
+        {
 
         }
-        //create UsersData table in DB reference column from model User
+        //create UsersData table in DB reference column from model 
         public DbSet<User> UsersData { get; set; }
+        public DbSet<GrabberPost> GrabberPostingField { get; set; }
+
     }
 }
