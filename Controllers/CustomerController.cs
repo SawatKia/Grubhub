6c586e6e3d5996e605fa4d3dfb490c11b8a0bba1
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-17 20:04:01 +0700

    add Graber and Customer controllers

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using Grubhub.Data;
+using Grubhub.Migrations;
+using Grubhub.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace Grubhub.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //DB representor
+        private readonly GrubhubDBContext _db;
+        public CustomerController(GrubhubDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index(User user_obj)
+        {
+            // Retrieve the list of UserId values from GrabberPostingField table
+            var userIds = _db.GrabberPostingField.Select(pf => pf.UserId).ToList();
+
+            // Retrieve the list of Id values from UsersData table
+            var ids = _db.UsersData.Select(u => u.Id).ToList();
+
+            // Find the userId values that don't exist in ids and remove their corresponding rows from GrabberPostingField table
+            var userIdsToRemove = userIds.Except(ids);
+            var postsToRemove = _db.GrabberPostingField.Where(pf => userIdsToRemove.Contains(pf.UserId));
+            _db.GrabberPostingField.RemoveRange(postsToRemove);
+            _db.SaveChanges();
+            var posts = _db.GrabberPostingField.ToList();
+            var viewModel = new UserPostsViewModel
+            {
+                User = user_obj,
+                Posts = posts
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Controllers/GrabberController.cs
@@ -0,0 +1,62 @@
+﻿using Grubhub.Data;
+using Grubhub.Migrations;
+using Grubhub.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grubhub.Controllers
+{
+    public class GrabberController : Controller
+    {
+        //DB representor
+        private readonly GrubhubDBContext _db;
+
+        public GrabberController(GrubhubDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index(User user)
+        {
+			var userId = user.Id; // replace with the actual user ID you want to search for
+			var posts = _db.GrabberPostingField
+				.Where(p => p.UserId == userId)
+				.ToList();
+
+			var viewModel = new UserPostsViewModel
+			{
+				User = user,
+				Posts = posts
+			};
+
+			return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(GrabberPost obj)
+        {
+            
+            User userData = _db.UsersData.FirstOrDefault(u => u.Id == obj.UserId);
+            obj.CloseTime = DateTime.MaxValue;
+            _db.GrabberPostingField.Add(obj);
+            _db.SaveChanges();
+
+            var posts = _db.GrabberPostingField
+                .Where(p => p.UserId == obj.UserId)
+                .ToList();
+
+            var viewModel = new UserPostsViewModel
+            {
+                User = userData,
+                Posts = posts
+            };
+            return View(viewModel);
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("LoginRegis", "User");
+        }
+
+    }
+}
