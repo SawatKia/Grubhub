e64480a31d9bcff1373cd28dcf86d7e31b3da9be
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-17 20:05:35 +0700

    add class for allow passing 2 objs to View
    it only can pass 1 obj

---
+++ Controllers/UserPostsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Grubhub.Models;
+using Grubhub.Controllers;
+
+namespace Grubhub.Controllers
+{
+    public class UserPostsViewModel
+    {
+        public User User { get; set; }
+        public List<GrabberPost> Posts { get; set; }
+    }
+}
