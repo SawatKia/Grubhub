739f2f0efe761b514b412ac5ce5ec3908aaca444
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-19 13:17:40 +0700

    backend final

--- Data/GrubhubDBContext.cs
+++ Data/GrubhubDBContext.cs
@@ -13,6 +13,7 @@ namespace Grubhub.Data
         //create UsersData table in DB reference column from model 
         public DbSet<User> UsersData { get; set; }
         public DbSet<GrabberPost> GrabberPostingField { get; set; }
+        public DbSet<CustomerOrder> CustomerOrders { get; set; }
 
     }
 }
