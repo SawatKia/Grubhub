9c336c2faee2ffde771913f31d320abcbb467c3c
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-19 13:22:30 +0700

    backend final

--- Program.cs
+++ Program.cs
@@ -1,17 +1,33 @@
-using bascicASP.Data;
-using Microsoft.Build.Framework;
+using Grubhub;
+using Grubhub.Data;
+using Microsoft.AspNetCore;
+//using System.Threading;
 using Microsoft.EntityFrameworkCore;
+using Grubhub.Controllers;
+using Grubhub.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//create a service that connect to SqlServer 
-builder.Services.AddDbContext<ApplicationDBContext>(
+//DB service to connect to DB sql server by GrubhubDBContext
+builder.Services.AddDbContext<GrubhubDBContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -28,8 +44,20 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseSession();
+
+//// create a new timer that calls the DeleteExpiredPosts method every hour
+//var timer = new System.Threading.Timer(callback: (state) =>
+//{
+//    using (var scope = app.Services.CreateScope())
+//{
+//        var controller = scope.ServiceProvider.GetService<GrabberController>();
+//        controller.DeleteExpiredPosts();
+//    }
+//}, state: null, dueTime: TimeSpan.Zero, period: TimeSpan.FromHours(1));
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Student}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=LoginRegis}/{id?}");
 
 app.Run();
