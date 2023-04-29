b78e8d706443978d6a0e28327c4fc94bc008db5c
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-29 16:50:43 +0700

    Add files via upload

---
+++ StaticFiles/Customer/customer-form_css2.js
@@ -0,0 +1,44 @@
+﻿function toggleForm(postId) {
+    var form = document.getElementById("order-form-" + postId);
+    if (form.style.display === 'none') {
+        form.style.display = 'flex';
+    } else {
+        form.style.display = 'none';
+    }
+
+}
+
+//form.querySelector('.close').addEventListener('click',
+//        function () {
+//            form.querySelector('.bg-model').style.display = 'none';
+//        });
+
+var bgModels = document.querySelectorAll('.bg-model');
+bgModels.forEach(function (bgModel) {
+    bgModel.addEventListener('click', function (event) {
+        // ตรวจสอบว่าคลิกที่ .close หรือไม่
+        if (event.target.classList.contains('close')) {
+            bgModel.style.display = 'none';
+        }
+    });
+});
+
+function validateForm(maxQuantity, maxPrice, postId) {
+    var numBoxes = parseInt(document.getElementById("numBoxes-" + postId).value);
+    var totalPrice = parseInt(document.getElementById("totalPrice-" + postId).value);
+    var warning = "";
+
+    if (numBoxes > maxQuantity) {
+        warning += "Number of boxes cannot exceed " + maxQuantity + ". ";
+    }
+    if (warning !== "") {
+        warning += " and "
+    }
+    if (totalPrice > maxPrice) {
+        warning += "Estimated total price cannot exceed " + maxPrice + ". ";
+    }
+    if (warning !== "") {
+        warning += ", please try again"
+        alert(warning);
+    }
+}
